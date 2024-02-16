e678f2d6931af38c8d0fde261de7dcf7cb8bbb31
Anand P <anandpamegoze@gmail.com>
2024-02-16 23:53:07 +0530

    4 . laying the foundation

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Create nested React Elements
-const heading = React.createElement(
-  'h1',
-  {
-    id: 'title',
-    style: {
-      background: 'red',
-    },
-    className: 'title',
-  },
-  'heading'
-);
-const heading1 = React.createElement(
-  'h1',
-  {
-    id: 'title',
-  },
-  'heading1'
-);
+// React.createElement => object => HTML(DOM)
+// const heading1 = React.createElement(
+//   'h1',
+//   {
+//     id: 'title',
+//     key: 'h1',
+//   },
+//   'Hello world'
+// );
 
-const container = React.createElement(
-  'div',
-  {
-    id: 'container',
-  },
-  [heading, heading1]
+// JSX => React.createElement => object => HTML(DOM)
+//it is a JSX expression
+//It is a react element
+const heading = (
+  <h1 id="title" key="h">
+    Namaste React
+  </h1>
 );
+const Title = () => {
+  return (
+    <h1 id="title" key="t">
+      Namaste React fro Title
+    </h1>
+  );
+};
+
+var a = 10;
+
+// React component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading}
+      {Title()}
+      {a}
+      <h1>h1 from HeaderComponent</h1>
+      <h2>h2 from HeaderComponent</h2>
+    </div>
+  );
+};
 
 // create root using createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // passing react element inside root
-root.render(container);
+root.render(<HeaderComponent />);
