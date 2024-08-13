26c6930d2e7657ef5fcaa8fccc42e5078147c520
Anand P <anandpamegoze@gmail.com>
2024-08-13 23:16:03 +0530

    update main shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,24 +1,21 @@
-import { shimmer_card_unit } from '../config';
-
-// Shimmer card to display with animation
-const CardShimmer = () => {
-  return (
-    <div className="shimmer-card">
-      <div className="shimmer-img stroke animate"></div>
-      <div className="shimmer-title stroke animate"></div>
-      <div className="shimmer-tags stroke animate "></div>
-      <div className="shimmer-details stroke animate "></div>
-    </div>
-  );
-};
-
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      {new Array(shimmer_card_unit).fill(0).map((element, index) => {
-        return <CardShimmer key={index} />;
-      })}
+    <div className="lg:mx-32 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {Array.from({ length: 20 }).map((_, index) => (
+        <div
+          key={index}
+          className="bg-white border border-gray-200 shadow-md rounded-lg p-4 animate-pulse"
+        >
+          <div className="h-48 bg-gray-200 rounded-lg mb-4 w-full"></div>
+          <div className="space-y-4">
+            <div className="w-full h-6 bg-gray-200 rounded"></div>
+            <div className="w-3/4 h-4 bg-gray-200 rounded"></div>
+            <div className="w-1/2 h-4 bg-gray-200 rounded"></div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
+
 export default Shimmer;
